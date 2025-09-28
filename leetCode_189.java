929698f64d76c97e27d777708cf49f918489fb60
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2025-09-28 14:33:15 +0530

    Add files via upload

---
+++ leetCode_189.java
@@ -0,0 +1,29 @@
+public class leetCode_189 {
+    public static void rotate(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+
+    public static void reverse(int[] arr, int left, int right) {
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 3;
+        rotate(arr, k);
+
+        for (int num : arr) {
+            System.out.println(num + " ");
+        }
+    }
+}

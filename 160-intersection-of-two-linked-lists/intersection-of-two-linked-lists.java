2b4e95e446b922289470eec60e182148a25b4fa8
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-07-04 22:55:33 +0530

    Time: 1 ms (99.94%) | Memory: 52.8 MB (44.51%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,40 +11,40 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode tempA = headA;
-        ListNode tempB = headB;
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
 
-        int lengthA = 0;
-        while(tempA != null){
-            lengthA++;
-            tempA = tempA.next;
+        int size1 = 0;
+        while(temp1 != null){
+            size1++;
+            temp1 = temp1.next;
         }
+        temp1 = headA;
+        int size2 =0;
+        while(temp2 != null){
+            size2++;
+            temp2 = temp2.next;
+        }
+        temp2 = headB;
 
-        int lengthB = 0;
-        while(tempB != null){
-            lengthB++;
-            tempB = tempB.next;
-        }
-        tempA = headA;
-        tempB = headB;
-        
-        if(lengthA > lengthB){
-            int step = lengthA - lengthB;
-            for(int i=0; i<step; i++){
-                tempA = tempA.next;
+        if(size1 > size2){
+            int diff1 = size1 - size2;
+            for(int i=0; i<diff1; i++){
+                temp1 = temp1.next;
             }
         }
         else{
-            int step = lengthB - lengthA;
-            for(int i=0; i<step; i++){
-                tempB = tempB.next;
+            int diff2 = size2 - size1;
+            for(int i=0; i<diff2; i++){
+                temp2 = temp2.next;
             }
         }
-        while(tempA != tempB){
-            tempA = tempA.next;
-            tempB = tempB.next;
-        }
-        return tempA;
 
+        while(temp1 != temp2){
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        return temp1;
+        
     }
 }
